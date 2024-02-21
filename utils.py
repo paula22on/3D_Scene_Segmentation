a9ac0959582833ec4026d2b2e46886506ee1c47c
albertpedra45 <pedra45@gmail.com>
2024-02-21 20:04:18 +0100

    change color to plots

--- utils.py
+++ utils.py
@@ -9,8 +9,8 @@ def compute_accuracy(pred, target):
 def plot_losses(train_loss, test_loss, save_to_file=None):
     fig = plt.figure()
     epochs = len(train_loss)
-    plt.plot(range(epochs), train_loss, 'b', label='Training loss')
-    plt.plot(range(epochs), test_loss, 'r', label='Validation loss')
+    plt.plot(range(epochs), train_loss, label='Training loss')
+    plt.plot(range(epochs), test_loss, label='Validation loss')
     plt.title('Training and validation loss')
     plt.legend()
     if save_to_file:
@@ -20,8 +20,8 @@ def plot_losses(train_loss, test_loss, save_to_file=None):
 def plot_accuracies(train_acc, test_acc, save_to_file=None):
     fig = plt.figure()
     epochs = len(train_acc)
-    plt.plot(range(epochs), train_acc, 'b', label='Training accuracy')
-    plt.plot(range(epochs), test_acc, 'r', label='Validation accuracy')
+    plt.plot(range(epochs), train_acc, label='Training accuracy')
+    plt.plot(range(epochs), test_acc, label='Validation accuracy')
     plt.title('Training and validation accuracy')
     plt.legend()
     if save_to_file:
