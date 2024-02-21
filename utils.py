3d8c23ff7de4335be71fb3841887ff1f503f02f9
albertpedra45 <pedra45@gmail.com>
2024-02-21 18:43:33 +0100

    Plotting results + save plot

--- utils.py
+++ utils.py
@@ -3,3 +3,25 @@ def compute_accuracy(pred, target):
     pred_choice = pred.max(1)[1]
     correct = pred_choice.eq(target.data).cpu().sum()
     return correct.item() / float(target.size(0) * target.size(1))
+
+
+def plot_losses(train_loss, test_loss, save_to_file=None):
+    fig = plt.figure()
+    epochs = len(train_loss)
+    plt.plot(range(epochs), train_loss, 'b', label='Training loss')
+    plt.plot(range(epochs), test_loss, 'r', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
+    if save_to_file:
+        fig.savefig(save_to_file,dpi=200)
+
+
+def plot_accuracies(train_acc, test_acc, save_to_file=None):
+    fig = plt.figure()
+    epochs = len(train_acc)
+    plt.plot(range(epochs), train_acc, 'b', label='Training accuracy')
+    plt.plot(range(epochs), test_acc, 'r', label='Validation accuracy')
+    plt.title('Training and validation accuracy')
+    plt.legend()
+    if save_to_file:
+        fig.savefig(save_to_file)
