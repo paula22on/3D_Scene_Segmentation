fe9a03012240b1e05b4475c41c7de8795457b699
Homet <laura.homet@hp.com>
2024-03-04 13:55:08 +0100

    Added weight calculation for inverse squared loss as well

--- dataset.py
+++ dataset.py
@@ -33,11 +33,8 @@ class MyDataset(Dataset):
         sample_path = f"{self.samples_path}/10_divisions_{idx}.csv"
         sample_df = pd.read_csv(sample_path, dtype=int)
 
-        # Pick n points
-        if self.sample_method == "augmentation":
-            sample_reduced = self.augmentation(sample_df)
-        else:
-            sample_reduced = sample_df.sample(self.number_points)
+        # Randomly pick n points
+        sample_reduced = sample_df.sample(self.number_points)
 
         # Separate point coordinates x, y, z from labels
         sample = sample_reduced.iloc[:, :3]
@@ -51,41 +48,7 @@ class MyDataset(Dataset):
 
         return sample, labels
 
-    def augmentation(self, dataframe):
-
-        sample = dataframe.values
-        labels = sample[:, -1]
-        label_distribution = Counter(labels)
-        average_num_points = int(np.mean(list(label_distribution.values())))
-
-        selected_points = []
-
-        # Iterate over the label distribution and select samples accordingly
-        for label, count in label_distribution.items():
-
-            # If the count is above the average A, randomly select A samples
-            if count > average_num_points:
-                label_rows = sample[labels == label]
-                selected_indices = np.random.choice(
-                    len(label_rows), size=average_num_points, replace=False
-                )
-                selected_points.extend(label_rows[selected_indices])
-
-            # If the count is below the average A, randomly select a sample and replicate it
-            elif count < average_num_points:
-                label_rows = sample[labels == label]
-                selected_indices = np.random.choice(
-                    len(label_rows), size=average_num_points - count, replace=True
-                )
-                selected_points.extend(label_rows)
-                selected_points.extend(label_rows[selected_indices])
-
-        sample = pd.DataFrame(selected_points)
-        sample_reduced = sample.sample(self.number_points)
-
-        return sample_reduced
-
-    def calculate_class_weights(self):
+    def calculate_class_weights(self, squared = False):
             label_distribution = Counter()
 
             for file in os.listdir(self.samples_path):
@@ -99,10 +62,17 @@ class MyDataset(Dataset):
             total_count = sum(
                 label_distribution.values()
             )  # calculates total amount of value sin each class
-            class_weights = {
-                class_label: total_count / (len(label_distribution) * count)
-                for class_label, count in label_distribution.items()
-            }  # total_count = total number samples, len(label_distr): number unique classes, count: instances in each class
+
+            if squared:
+                class_weights = {
+                    class_label: total_count / (count**2)
+                    for class_label, count in label_distribution.items()
+                }  # total_count = total number samples, count: instances in each class
+            else:
+                class_weights = {
+                    class_label: total_count / (len(label_distribution) * count)
+                    for class_label, count in label_distribution.items()
+                }  # total_count = total number samples, len(label_distr): number unique classes, count: instances in each class
 
             # Convert the weights into a sorted list based on the class labels -- we need to make sure that each weight corresponds to each class!!
             weights = [class_weights[i] for i in range(len(class_weights))]
